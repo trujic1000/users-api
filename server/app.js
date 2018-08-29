29d1d0b5282d5253b4e837770039cb91d44fb7b3
trujic1000 <aleksandar.trujic@live.com>
2018-08-28 21:30:34 -0500

    GET /users added

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
+
 const { mongoose } = require('./db/mongoose');
+const userRouter = require('./routers/userRouter');
+const User = require('./models/user');
 
 const port = process.env.PORT || 3000;
 
@@ -7,6 +10,28 @@ const app = express();
 
 // Middlewares
 require('./middlewares/middlewares')(app);
+
+// API
+// app.use('/users', userRouter);
+app.get('/users', (req, res) => {
+  res.send('Users');
+});
+app.post('/users', (req, res) => {
+  // Destructure email and password from request
+  const { email, password } = req.body;
+  // Create new user
+  const user = new User({ email, password });
+
+  // Save user to db
+  (async function saveUser() {
+    try {
+      await user.save();
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(400).send();
+    }
+  }());
+});
 
 // Starting the server
 app.listen(port, () => {
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlParser', true);
 mongoose.connect('mongodb://localhost:27017/users-api');
 
 module.exports = {
